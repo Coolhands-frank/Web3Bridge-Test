efaf5aea82598d17b235422c5c0abd0251c4a22a
Elendu Franklin <elendufranklin66@gmail.com>
2022-10-28 19:06:47 +0100

    added Javascript and workings

--- reg.js
+++ reg.js
@@ -1,6 +1,29 @@
 let userName = document.querySelector("#name")
-let email = document.querySelector("#Email")
+let email = document.querySelector("#email")
+let emailError = document.querySelector("#email-error")
+let usernameError = document.querySelector("#username-error")
 
-userName.addEventListener("input", function(e) {
-    
+email.addEventListener("input", function(e) {
+    console.log(e.target.value)
+    var pattern = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    var inValue = e.target.value;
+    var valid = pattern.test(inValue);
+
+    if(valid) {
+        emailError.style.display = 'block'
+    } else {
+        emailError.style.display = 'none'
+    }
+})
+
+UserName.addEventListener("input", function(e) {
+    var pattern = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    var inValue = e.target.value;
+    var valid = pattern.test(inValue);
+
+    if(valid) {
+        usernameError.style.display = 'block'
+    } else {
+        usernameError.style.display = 'none'
+    }
 })
