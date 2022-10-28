6b93f985781ac00d386d0488a015d64c7b0f7d09
Elendu Franklin <elendufranklin66@gmail.com>
2022-10-28 19:14:51 +0100

    update

--- reg.js
+++ reg.js
@@ -5,25 +5,32 @@ let usernameError = document.querySelector("#username-error")
 
 email.addEventListener("input", function(e) {
     console.log(e.target.value)
-    var pattern = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-    var inValue = e.target.value;
-    var valid = pattern.test(inValue);
+    let pattern = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    let inValue = e.target.value;
+    console.log(pattern.test(inValue))
+
+    let valid = pattern.test(inValue);
 
     if(valid) {
-        emailError.style.display = 'block'
-    } else {
         emailError.style.display = 'none'
+    } else {
+        emailError.style.display = 'block'
     }
 })
 
 UserName.addEventListener("input", function(e) {
-    var pattern = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-    var inValue = e.target.value;
-    var valid = pattern.test(inValue);
+    let pattern = /^[A-Z]/;
+    let inValue = e.target.value;
+
+    console.log(pattern.test(inValue))
+
+    let valid = pattern.test(inValue);
+
+    
 
     if(valid) {
-        usernameError.style.display = 'block'
-    } else {
         usernameError.style.display = 'none'
+    } else {
+        usernameError.style.display = 'block'
     }
 })
